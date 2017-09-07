a061bac05cafef1adbf12c1da967ecfba8ea913f
ayushmaan <ayushmaan.lavania@gaussnetworks.com>
2017-09-07 16:27:35 +0530

    Add project files.

---
+++ App15/ViewController1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UIKit;
+
+namespace App15
+{
+    public partial class ViewController1 : UIViewController
+    {
+        public ViewController1() : base("ViewController1", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
